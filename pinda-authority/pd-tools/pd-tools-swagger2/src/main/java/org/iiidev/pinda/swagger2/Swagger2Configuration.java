d21eeafc641670b3b5c861550586816e29a74458
IIIDelay <IIIDev@.com>
2023-06-27 23:54:30 +0800

    提交信息: swagger 404解决

--- pinda-authority/pd-tools/pd-tools-swagger2/src/main/java/org/iiidev/pinda/swagger2/Swagger2Configuration.java
+++ pinda-authority/pd-tools/pd-tools-swagger2/src/main/java/org/iiidev/pinda/swagger2/Swagger2Configuration.java
@@ -38,10 +38,13 @@ public class Swagger2Configuration extends WebMvcConfigurationSupport{
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars*")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 
 }
